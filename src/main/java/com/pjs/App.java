49988f79527cd62f91a4c6c9e6b889722d541c42
pjs2k <pauljoesinnott@gmail.com>
2017-10-03 20:41:18 +0100

    03102017 v1 commit

---
+++ src/main/java/com/pjs/App.java
@@ -0,0 +1,13 @@
+package com.pjs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
